caec0e984f3057dd83f3eaf38d09bf3d7bac0682
KyTranMoi <21011801.ky@student.iuh.edu.com>
2024-09-03 22:11:45 +0700

    chore: Add script to run mainbonbanh.py and mainchotot.py

---
+++ runscript.bat
@@ -0,0 +1,7 @@
+@echo off
+cd C:\Users\Administrator\bts
+call C:\Users\Administrator\bts\.venv\Scripts\activate
+python C:\Users\Administrator\bts\mainbonbanh.py
+python C:\Users\Administrator\bts\mainchotot.py
+deactivate
+pause
