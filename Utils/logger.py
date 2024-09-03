0e3396331cab78aaf38b1387a3e488accfe75c57
KyTranMoi <21011801.ky@student.iuh.edu.com>
2024-09-03 21:45:55 +0700

    chore: Update logger.py to include timestamp in log messages

--- Utils/logger.py
+++ Utils/logger.py
@@ -1,9 +1,11 @@
 import os
 import sys
+from datetime import datetime
 sys.path.append('../')
-
+def get_time_now():
+    return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 def logger(path_log, text):
     os.makedirs(f'{"/".join(path_log.split("/")[:-1])}', exist_ok=True)
     with open(f'{path_log}', 'a+', encoding='utf-8') as f:
-        f.write(str(text) + '\n')
+        f.write(str(text)+' - '+str(get_time_now()) + '\n')
         f.close()
